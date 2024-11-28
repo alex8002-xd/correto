2f687c6cda30c3f252ae3e4bf2ec5ef5f5a30245
Devanyrdz12 <devany.rodriguez.alp@cbtis258.edu.mx>
2024-11-27 22:09:30 -0600

    Update ww.js

--- ww.js
+++ ww.js
@@ -1,41 +1,32 @@
 let orden_correcto = ['when', 'while', 'when', 'while'];
-
 let palabras_juego = [ 'while','when','when','while'];
-
 let contenedorOpciones = document.getElementById("opciones");
-
 let comprobar = document.getElementById("comprobar");
-
 let txtResultado = document.getElementById("resultado");
+let posDisponible = ["", "", "", ""];
 
-let posDisponible = ["","","","",];
-
-function agregaOpciones(){
-palabras_juego.forEach(Element => {
-let div = document.createElement("div")
-div.className = "palabra";
-div.innerHTML = Element;
-div.setAttribute("onclick", "completar(this)");
-contenedorOpciones.appendChild(div);
-});
+function agregarOpciones(){
+    palabras_juego.forEach(element => {
+        let div = document.createElement("div");
+        div.className = "palabra";
+        div.innerHTML = element;
+        div.setAttribute("onclick", "completar(this)");
+        contenedorOpciones.appendChild(div);
+    });
 }
-
 agregarOpciones();
-
 function agregarEliminarAspan(){
-var spans = document.getElementsByTagName("span");
-for(let i=0; i < spans.lenght; i++){
-    spans[i].setAttribute("onclick", "remove(this)");
-}
+    var spans = document.getElementsByTagName("span");
+    for(let i=0; i < spans.length; i++){
+        spans[i].setAttribute("onclick", "remove(this)");
+    }
 }
 agregarEliminarAspan();
-
 function completar(palabra){
-let posLibre = posDisponible.indexOf("");
-document.getElementById(posLibre).innerHTML = palabra.
-innerHTML;
-posDisponible[posLibre] = palabra.innerHTML;
-contenedorOpciones.removeChild(palabra);
+    let posLibre = posDisponible.indexOf("");
+    document.getElementById(posLibre).innerHTML = palabra.innerHTML;
+    posDisponible[posLibre] = palabra.innerHTML;
+    contenedorOpciones.removeChild(palabra);
 }
 
 function remove(palabra){
@@ -45,12 +36,10 @@ function remove(palabra){
         div.innerHTML = palabra.innerHTML;
         div.setAttribute("onclick", "completar(this)");
         contenedorOpciones.appendChild(div);
-
         palabra.innerHTML = "";
         posDisponible[palabra.id] = "";
 
-        document.getElementById(palabra.id).style.background = 
-        "#ccc";
+        document.getElementById(palabra.id).style.background = "#ccc";
 
         txtResultado.innerHTML = "";
     }
@@ -60,23 +49,23 @@ comprobar.onclick = function(){
     let posLibre = posDisponible.indexOf("");
     let totalAciertos = 0;
 
-    if(poslibre==-1){
+    if(posLibre==-1){
         for(i=0; i <orden_correcto.length;i++){
             if(orden_correcto[i]==posDisponible[i]){
-                document.getElementById(i).style.background =
-                "#c0ff33";
+                document.getElementById(i).style.background = "#c0ff33";
+                totalAciertos++;
             }else{
-                document.getElementById(i).style.background =
-                "#fb4b4b";
+                document.getElementById(i).style.background = "#fb4b4b";
             }
         }
 
         if(totalAciertos==orden_correcto.length){
-            txtResultado.innerHTML = "¡Muy bien!";
+            txtResultado.innerHTML = "Muy bien!!";
         }else{
-            txtResultado.innerHTML = "¡Tienes errores!";
+            txtResultado.innerHTML = "Existen errores!!";
         }
     }else{
-        alert("Completa los espacios faltantes");
+        alert("Completa las palabras");
     }
+
 }
